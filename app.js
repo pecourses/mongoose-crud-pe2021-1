0bedf92401965b6008d025893f5867637436ba4e
Viktoriia <pecourses@gmail.com>
2021-10-17 16:09:31 +0300

    Add server, app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return;
+  }
+  res
+    .status(err?.status ?? 500)
+    .send({ errors: [{ title: err?.message ?? 'Internal server error' }] });
+});
+
+module.exports = app;
