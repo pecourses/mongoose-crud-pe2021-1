c7250a9a175120b66c1b00bad6e6809864e753eb
Viktoriia <pecourses@gmail.com>
2021-10-17 16:53:27 +0300

    Add connection

---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  development: {
+    host: '127.0.0.1',
+    port: 27017,
+    dbName: 'test',
+  },
+};
---
+++ models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const env = process.env.NODE_ENV ?? 'development';
+const config = require('./../config/db')[env];
+
+mongoose
+  .connect(`mongodb://${config.host}:${config.port}/${config.dbName}`)
+  .then(data => console.log(`Connection OK`))
+  .catch(err => console.log(`err`, err));
