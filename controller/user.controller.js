011bad4e74633b436a515e81a955494255301d43
Viktoriia <pecourses@gmail.com>
2021-10-17 16:23:19 +0300

    Add controller

---
+++ controller/user.controller.js
@@ -0,0 +1,19 @@
+module.exports.createUser = async (req, res, next) => {
+  console.log(`Ok`);
+};
+
+module.exports.getUsers = async (req, res, next) => {
+  console.log(`Ok`);
+};
+
+module.exports.getUserById = async (req, res, next) => {
+  console.log(`Ok`);
+};
+
+module.exports.updateUserById = async (req, res, next) => {
+  console.log(`Ok`);
+};
+
+module.exports.deleteUserById = async (req, res, next) => {
+  console.log(`Ok`);
+};
