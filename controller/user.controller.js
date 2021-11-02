5af4fc2660ed3131656e6385ca0924619eef5593
Viktoriia <pecourses>
2021-11-02 10:22:58 +0200

    App post controllers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
-const { User } = require('./../models');
+const mongoose = require('mongoose');
+const { User, Post } = require('./../models');
 
 module.exports.createUser = async (req, res, next) => {
   const { body } = req;
@@ -51,11 +52,15 @@ module.exports.updateUserById = async (req, res, next) => {
   } = req;
 
   try {
-    const updatedUser = await User.findByIdAndUpdate(userId, body);
+    // new:true -- возвращать новое (обновленное) значение
+    // runValidators:true -- применять валидацию при обновлении
+    const updatedUser = await User.findOneAndUpdate({ _id: userId }, body, {
+      new: true,
+      runValidators: true,
+    });
 
     if (updatedUser) {
-      return next();
-      //return res.status(200).send({ data: updatedUser });
+      return res.status(200).send({ data: updatedUser });
     }
     next(createError(404, 'User not found'));
   } catch (err) {
@@ -73,6 +78,67 @@ module.exports.deleteUserById = async (req, res, next) => {
       return res.status(200).send({ data: deletedUser });
     }
     next(createError(400, 'User not found'));
+  } catch (err) {
+    next(err);
+  }
+};
+
+// POST на путь 'http://HOST:PORT/api/users/:userId/posts'
+// создание поста конкретного юзера (если такой юзер есть)
+module.exports.createUserPost = async (req, res, next) => {
+  const {
+    params: { userId },
+    body,
+  } = req;
+
+  try {
+    const foundUser = await User.findById(userId);
+    if (foundUser) {
+      const newPostInstanse = new Post({
+        ...body,
+        userId: mongoose.Types.ObjectId(userId),
+      });
+
+      const createdPost = await newPostInstanse.save(newPostInstanse);
+      if (createdPost) {
+        return res.status(201).send(createdPost);
+      }
+      next(createError(400, 'Bad request'));
+    }
+
+    next(createError(404, 'User Not Found'));
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET на путь 'http://HOST:PORT/api/users/:userId/posts'
+// отравка постов конкретного юзера (если такой юзер есть)
+module.exports.getUserPosts = async (req, res, next) => {
+  const {
+    params: { userId },
+  } = req;
+
+  try {
+    const foundUser = await User.findById(userId);
+    if (foundUser) {
+      // aggregate (+ match, lookup, project)
+      // работает аналогично aggregate в MongoDB:
+      // - match - фильтр,
+      // - lookup - соединение с документами другой коллекции по указанным критериям
+      // - project - проекция (какие поля полученных документов включать в результат)
+      const foundUserPosts = await User.aggregate()
+        .match({ _id: mongoose.Types.ObjectId(userId) })
+        .lookup({
+          from: 'posts',
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'userPosts',
+        })
+        .project({ _id: 0, userPosts: { body: 1, createdAt: 1 } });
+      res.status(200).send(foundUserPosts);
+    }
+    next(createError(404, 'User Not Found'));
   } catch (err) {
     next(err);
   }
