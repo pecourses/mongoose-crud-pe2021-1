2ff0612ba882d7a6560d3870d5ff7ef5d7d7a4bd
Viktoriia <pecourses>
2021-11-02 10:22:27 +0200

    Add post routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -11,7 +11,12 @@ userRouter
 userRouter
   .route('/:userId')
   .get(userController.getUserById)
-  .patch(userController.updateUserById, userController.getUserById)
+  .patch(userController.updateUserById)
   .delete(userController.deleteUserById);
+
+// создать пост конкретного юзера
+userRouter.post('/:userId/posts', userController.createUserPost);
+// получить посты конкретного юзера
+userRouter.get('/:userId/posts', userController.getUserPosts);
 
 module.exports = userRouter;
